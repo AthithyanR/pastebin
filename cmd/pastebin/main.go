2f88140f0b101a9eed56f5b9d4a6942b5fd84bd9
Athithyan Rengasamy <athithyan@crystal.local>
2024-01-22 00:42:35 +0530

    base impl complete

--- cmd/pastebin/main.go
+++ cmd/pastebin/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"io"
 	"net/http"
+	"os"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	e := echo.New()
@@ -13,11 +26,48 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", hello)
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = t
+
+	e.GET("/", index)
+	e.GET("/paste/:id", getPaste)
+	e.POST("/paste", postPaste)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
+func index(c echo.Context) error {
+	dirEntry, err := os.ReadDir("pastes")
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	var pastes []string
+	for _, entry := range dirEntry {
+		pastes = append(pastes, entry.Name())
+	}
+	return c.Render(http.StatusOK, "index", pastes)
+}
+
+func getPaste(c echo.Context) error {
+	contentId := c.Param("id")
+	content, err := os.ReadFile(fmt.Sprintf("pastes/%s", contentId))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.String(http.StatusOK, string(content))
+}
+
+func postPaste(c echo.Context) error {
+	content := c.FormValue("content")
+	contentId := uuid.New().String()
+
+	if err := os.WriteFile(fmt.Sprintf("pastes/%s", contentId), []byte(content), 0644); err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.String(http.StatusOK, fmt.Sprintf("<a href=\"/paste/%s\">%s<a>", contentId, contentId))
 }
